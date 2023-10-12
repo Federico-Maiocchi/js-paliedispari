c03ec7234250a74fc45eb468c2149538cec745eb
Federico-Maiocchi <maiocchifederico@gmail.com>
2023-10-12 16:24:46 +0200

    ADD: function Ispalindrome

--- js/palinfromi.js
+++ js/palinfromi.js
@@ -4,23 +4,40 @@
 let parolaUtente = prompt('inserisci una parola');
 console.log('Parola utente inserita',parolaUtente);
 
+
+
 // inizializzo una variabile che corrisponderà alla lunghezza della parola che la mia function analizzerà
 const lunghezzaParolaUtente = parolaUtente.length;
 // console.log(lunghezzaParolaUtente);
 
-// convertire una parola in un array
+// convertire una stringa in un array
 let arrayParolaUtente = parolaUtente.split('')
 console.log(arrayParolaUtente)
 
+// usiamo un array REVERSE per invertire l'ordine degli elementi
 let arrayReverseParolaUtente = arrayParolaUtente.reverse();
 console.log(arrayReverseParolaUtente)
 
-
-for ( let i = 0; i < lunghezzaParolaUtente; i++) {
-    const char = parolaUtente.charAt(i)
-    // console.log(char)
-
+function Ispalindrome(array, arrayReverse){
+    if (array = arrayReverse) {
+        return true 
+    } else {
+        return false
+    }
 }
+
+if (Ispalindrome(arrayParolaUtente, arrayReverseParolaUtente)) {
+    console.log('è palindroma')    
+} else {
+    console.log('non è palindroma')
+}
+
+
+// for ( let i = 0; i < lunghezzaParolaUtente; i++) {
+//     const char = parolaUtente.charAt(i)
+//     // console.log(char)
+
+// }
 
 
 // const array = [char]
