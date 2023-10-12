990ef4dbec17c939870c056ecd394436f3903313
Federico-Maiocchi <maiocchifederico@gmail.com>
2023-10-12 14:23:23 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Capire se la parola è palindroma
+
+- chiedere all' utente di digitare la parola
+    - la parola verrà associata ad una variabile
+
+- creare una function che ci permetta di capire se la parola è palindroma
---
+++ js/palinfromi.js
@@ -0,0 +1,7 @@
+// console.log('Palindromi')
+
+// // Con un prompt chiede allo utente di inserire una parola
+// const parolaUtente = prompt('inserisci una parola');
+// console.log(parolaUtente);
+
+// const lunghezzaParolaUtente = parolaUtente.length;
