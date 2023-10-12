5aeea02906f8c2b6187aab5c8ce63771374fb4a4
Federico-Maiocchi <maiocchifederico@gmail.com>
2023-10-12 15:48:17 +0200

    ADD: convert string and arrayrevers

--- js/palinfromi.js
+++ js/palinfromi.js
@@ -1,7 +1,30 @@
 // console.log('Palindromi')
 
-// // Con un prompt chiede allo utente di inserire una parola
-// const parolaUtente = prompt('inserisci una parola');
-// console.log(parolaUtente);
+// Con un prompt chiede allo utente di inserire una parola
+let parolaUtente = prompt('inserisci una parola');
+console.log('Parola utente inserita',parolaUtente);
 
-// const lunghezzaParolaUtente = parolaUtente.length;
+// inizializzo una variabile che corrisponderà alla lunghezza della parola che la mia function analizzerà
+const lunghezzaParolaUtente = parolaUtente.length;
+// console.log(lunghezzaParolaUtente);
+
+// convertire una parola in un array
+let arrayParolaUtente = parolaUtente.split('')
+console.log(arrayParolaUtente)
+
+let arrayReverseParolaUtente = arrayParolaUtente.reverse();
+console.log(arrayReverseParolaUtente)
+
+
+for ( let i = 0; i < lunghezzaParolaUtente; i++) {
+    const char = parolaUtente.charAt(i)
+    // console.log(char)
+
+}
+
+
+// const array = [char]
+// console.log(array)
+
+// funzione per capire capire se la parola dell'utente è palindroma
+// ciao
