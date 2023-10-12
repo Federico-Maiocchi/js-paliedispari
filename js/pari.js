42a14dae37f8449062f50746557c851d38ef8f53
Federico-Maiocchi <maiocchifederico@gmail.com>
2023-10-12 19:00:24 +0200

    ADD: function isEven

--- js/pari.js
+++ js/pari.js
@@ -6,7 +6,7 @@ const sceltaUtente = prompt('Scegli tra pari o dispari');
 const numeroUtente = parseInt(prompt('scegli un numero da 1 a 5'));
 console.log(typeof numeroUtente, numeroUtente, 'num. utente');
 
-// la scelta dei numiri sarà random
+// la scelta del pc sarà random
 
 let numRandom
 
@@ -19,4 +19,29 @@ function getRandomInt(min, max) {
 // numero random del pc
 numRandom = getRandomInt(1, 6);
 console.log(numRandom)
-// ci sarà un'altra funzione che cacolerà la somma dei numneri casuali se è pari o dispari si vedrà chi ha vinto
+
+
+// fare la somma dei due numeri TOTALE
+const totale = numeroUtente + numRandom;
+console.log('totale conteggio',totale)
+
+// funzione che indicherà il risultato se sarà pari o dispari
+function isEven(num) {
+
+    if (num % 2 === 0) {
+        return true
+
+    }
+
+    return false
+}
+
+// console.log che indicherà se pari o dispari il risultato
+if (isEven(totale)) {
+    console.log('pari')
+} else {
+    console.log('dispari')
+}
+
+// decretare il vincitore in base alle scelte
+
